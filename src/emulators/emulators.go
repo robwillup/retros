4b8203cb39ba7d355ff6ddaa6ba8b57fd84c3193
Robson William <30180338+robwillup@users.noreply.github.com>
2024-04-22 17:09:01 -0300

    Fixing some emulator names

--- src/emulators/emulators.go
+++ src/emulators/emulators.go
@@ -4,64 +4,58 @@ import "path/filepath"
 
 func FindEmulatorFromExtension(romFile string) string {
 	switch filepath.Ext(romFile) {
+	case ".32x":
+		return "sega32x"
 	case ".a26":
 		return "atari2600"
-	case ".a78":
-		return "atari7800"
 	case ".a52":
 		return "atari5200"
+	case ".a78":
+		return "atari7800"
+	case ".col":
+		return "coleco"
+	case ".gb":
+		return "gb"
+	case ".gba":
+		return "gba"
+	case ".gbc":
+		return "gbc"
+	case ".gcm":
+		fallthrough
+	case ".gcz":
+		return "gc"
+	case ".gen":
+		return "genesis"
+	case ".gg":
+		return "gamegear"
 	case ".j64":
 		fallthrough
 	case ".jag":
 		return "atarijaguar"
 	case ".lnx":
 		return "atarilynx"
-	case ".st":
-		fallthrough
-	case ".stx":
-		return "atarist"
-	case ".col":
-		return "coleco"
-	case ".gba":
-		return "gba"
-	case ".gbc":
-		return "gbc"
-	case ".gb":
-		return "gb"
-	case ".gcm":
-		fallthrough
-	case ".gcz":
-		return "gc"
-	case ".gg":
-		return "gamegear"
-	case ".gen":
-		fallthrough
 	case ".md":
 		return "megadrive"
-	case ".sms":
-		return "mastersystem"
-	case ".z64":
-		fallthrough
-	case ".v64":
-		fallthrough
 	case ".n64":
 		return "n64"
 	case ".nds":
 		return "nds"
-	case ".fds":
-		fallthrough
 	case ".nes":
 		return "nes"
-	case ".32x":
-		return "sega32x"
-	case ".cue":
-		fallthrough
-	case ".chd":
-		return "segacd"
+	case ".pce":
+		return "pcengine"
 	case ".sfc":
 		fallthrough
 	case ".smc":
 		return "snes"
+	case ".sg":
+		return "sg-1000"
+	case ".sms":
+		return "mastersystem"
+	case ".st":
+		fallthrough
+	case ".stx":
+		return "atarist"
 	default:
 		return ""
 	}
