ce2739a6d05351024f10a85249b5a139f8f1da1e
Robson William <30180338+robwillup@users.noreply.github.com>
2026-02-09 21:18:26 -0300

    fix: checksum tests failed in pipeline

--- internal/checksum/calculator_test.go
+++ internal/checksum/calculator_test.go
@@ -4,15 +4,15 @@ import "testing"
 
 //goland:noinspection SpellCheckingInspection
 var checksum = ROM{
-	Name:   "main.go",
-	MD5:    "b7beefafe8c7bfc37e2f127465fdc60d",
-	SHA1:   "c82b6fd625357a04fde423f6580b677c5fab5e6d",
-	SHA256: "a8cb0bb73c28ba4b79a76f161315eaad734af2bffc6000a91cbb93925d63d9a7",
-	Size:   1164,
+	Name:   "calculator.go",
+	MD5:    "83e321154c56b253443bb031759e4b23",
+	SHA1:   "1b6ff79552dc91d638b867064012f028f4e7e096",
+	SHA256: "3dacea3020cbdd866d2c1156bedaf5f065cfd37f5516aed64258fb6bf247b9e0",
+	Size:   895,
 }
 
 func TestCalcChecksum(t *testing.T) {
-	actual, err := CalcChecksum("../../cmd/retros/main.go")
+	actual, err := CalcChecksum("calculator.go")
 
 	if err != nil {
 		t.Fatalf("Failed to TestCalcChecksum().\nError: %v\n", err)
